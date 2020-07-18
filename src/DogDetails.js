eeb80510a948d3c08f554b05adb82e4fcd700bf6
von Buelow <jaspar@UMICH.EDU>
2020-07-18 17:35:56 -0400

    DogDetails component

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-
-function DogDetails() {
+function DogDetails({dogs}) {
+    const { name } = useParams();
+    const [dog] = dogs.filter(dog => dog.name.toLowerCase() === name);
+     
     return (
-        <div>
-
+        <div className="DogDetails">
+            <h1>{dog.name}</h1>
+            <p>Age: {dog.age}</p>
+            <img src={dog.src}/>
+            <ul>
+                {dog.facts.map(fact => <li>{fact}</li>)}
+            </ul>
         </div>
     );
 }
